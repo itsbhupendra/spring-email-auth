ff85a82d7c00f5b56d1e0b155d6217911fb9a801
itsbhupendra <bhupendrasingh12398@gmail.com>
2021-09-12 21:49:44 +0530

    Initial commit

---
+++ src/main/java/com/example/springemailauthentication/SpringEmailAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springemailauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmailAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springemailauthentication/SpringEmailAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springemailauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
